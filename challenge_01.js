ade70d76e81c06ec1d9c0d0ba805dc50412ba1bd
Marco <kranz-marco@gmx.de>
2021-02-08 15:16:50 +0100

    Variante I / Typenkonventierung

--- challenge_01.js
+++ challenge_01.js
@@ -9,18 +9,25 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
 
-
-/*let number1, number2, summe;
-number1 = prompt(" Bitte geben Sie 1. Zahl ein! ");
-number2 = prompt(" Bitte geben Sie 2. Zahl ein! ")
-let summe = number1 + number2;
-console.log(" Die Summe der Zahlen ist = " summe + number2 + " " + number1 + " """);
-
 //************** Variante I ***************/
 
-let zahl1, zahl2, Summe;
-zahl1 = 2;
-zahl2 = 2;
+// Deklaration
+let ziffer1, ziffer2;
+let zahl1, zahl2, summe;
+
+// Eingabe :: Ziffern
+ziffer1 = prompt(" Bitte Zahl 1 eingeben "); // 2
+ziffer2 = prompt("Bitte Zahl 2 eingeben "); // 2
+console.log(typeof ziffer1);
+console.log(typeof ziffer2);
+
+// Typenkonventierung / Ziffer --> Zahl (string --> number)
+zahl1 = parseInt(ziffer1);
+zahl2 = parseInt(ziffer2);
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+// Berechnung :: Summe
 summe = (zahl1 + zahl2);
 console.log(" Die Summe ist: " + summe);
 
