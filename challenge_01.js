1a1a1b5cbf67cb861da6a44a66e6e9fcaaba5d82
Marco <kranz-marco@gmx.de>
2021-02-08 14:35:56 +0100

    Variante 1/1 (2+2)

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,20 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
 
+
+/*let number1, number2, summe;
+number1 = prompt(" Bitte geben Sie 1. Zahl ein! ");
+number2 = prompt(" Bitte geben Sie 2. Zahl ein! ")
+let summe = number1 + number2;
+console.log(" Die Summe der Zahlen ist = " summe + number2 + " " + number1 + " """);
+
+//************** Variante I ***************/
+
+let zahl1, zahl2, Summe;
+zahl1 = 2;
+zahl2 = 2;
+summe = (zahl1 + zahl2);
+console.log(" Die Summe ist: " + summe);
+
+
+
